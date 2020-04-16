69b22b4d5c2909c6eff7742d3e71383992c8bf31
KelliePetersen <kelliepetersen@outlook.com>
2020-04-16 10:00:00 -0600

    add custom logo from wordpress backend

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function myfirsttheme_theme_support() {
   add_theme_support('title-tag');
+  add_theme_support('custom-logo');
 }
 
 add_action('after_setup_theme', 'myfirsttheme_theme_support');
