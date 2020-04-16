55d26b2537fcfc2cd47a74fe155140ee287946ed
KelliePetersen <kelliepetersen@outlook.com>
2020-04-16 10:00:00 -0600

    update footer copyright

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <footer class="footer text-center py-2 theme-bg-dark">
-      <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+      <p class="copyright">&copy; Copyright 2020. All rights reserved.</p>
     </footer>
   </div>
  
