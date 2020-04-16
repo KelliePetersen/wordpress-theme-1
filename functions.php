a311a66c228553af0ceed19a42aa95154249dbc4
KelliePetersen <kelliepetersen@outlook.com>
2020-04-16 12:00:00 -0600

    set up and style dynamic comments

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function myfirsttheme_theme_support() {
   add_theme_support('title-tag');
   add_theme_support('custom-logo');
+  add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'myfirsttheme_theme_support');
